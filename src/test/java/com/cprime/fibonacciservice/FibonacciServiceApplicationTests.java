35fa7af5e21c2f0c793a685f632f8069b6318f1b
Kevin Ryan <kevin@pulpdigital.com>
2021-10-11 12:50:37 +0000

    api tests for internal server errors

--- src/test/java/com/cprime/fibonacciservice/FibonacciServiceApplicationTests.java
+++ src/test/java/com/cprime/fibonacciservice/FibonacciServiceApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.junit.jupiter.api.Assertions;
+import org.json.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class FibonacciServiceApplicationTests {
@@ -36,6 +37,45 @@ class FibonacciServiceApplicationTests {
 		FibonacciNumber response = this.restTemplate.getForObject("/fibonacci/0", FibonacciNumber.class);
 		Assertions.assertEquals(0, response.getFibonacciIndex());
 		Assertions.assertEquals(0, response.getFibonacciValue());
+	}
+
+	@Test
+	void twentyFiveIndexRequestReturnsCorrectValue() {
+		FibonacciNumber response = this.restTemplate.getForObject("/fibonacci/25", FibonacciNumber.class);
+		Assertions.assertEquals(25, response.getFibonacciIndex());
+		Assertions.assertEquals(75025L, response.getFibonacciValue());
+	}
+
+	@Test
+	void fiftyIndexRequestReturnsCorrectValue() {
+		FibonacciNumber response = this.restTemplate.getForObject("/fibonacci/50", FibonacciNumber.class);
+		Assertions.assertEquals(50, response.getFibonacciIndex());
+		Assertions.assertEquals(12586269025L, response.getFibonacciValue());
+	}
+
+	@Test
+	void seventyFiveIndexRequestReturnsCorrectValue() {
+		FibonacciNumber response = this.restTemplate.getForObject("/fibonacci/75", FibonacciNumber.class);
+		Assertions.assertEquals(75, response.getFibonacciIndex());
+		Assertions.assertEquals(2111485077978050L, response.getFibonacciValue());
+	}
+
+	@Test
+	void seventySixIndexRequestThrowsInternalServerError() throws JSONException {
+		String response = this.restTemplate.getForObject("/fibonacci/76", String.class);
+		JSONObject object = new JSONObject(response);
+		Assertions.assertEquals(500, object.getInt("status"));
+		Assertions.assertEquals("Internal Server Error", object.getString("error"));
+		Assertions.assertEquals("/fibonacci/76", object.getString("path"));
+	}
+
+	@Test
+	void minusOneIndexRequestThrowsInternalServerError() throws JSONException {
+		String response = this.restTemplate.getForObject("/fibonacci/-1", String.class);
+		JSONObject object = new JSONObject(response);
+		Assertions.assertEquals(500, object.getInt("status"));
+		Assertions.assertEquals("Internal Server Error", object.getString("error"));
+		Assertions.assertEquals("/fibonacci/-1", object.getString("path"));
 	}
 
 }
