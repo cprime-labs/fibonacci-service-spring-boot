b043291b568b70c6702abb70264c993530b5fc6a
Kevin Ryan <kevin@pulpdigital.com>
2021-10-11 12:16:43 +0000

    add first api test

--- src/test/java/com/cprime/fibonacciservice/FibonacciServiceApplicationTests.java
+++ src/test/java/com/cprime/fibonacciservice/FibonacciServiceApplicationTests.java
@@ -2,13 +2,25 @@ package com.cprime.fibonacciservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.junit.jupiter.api.Assertions;
 
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class FibonacciServiceApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void rootRequestReturnsSystemUpMessage() {
+		FibonacciServiceStatus response = this.restTemplate.getForObject("/", FibonacciServiceStatus.class);
+		Assertions.assertEquals("UP", response.getStatus());
 	}
 
 }
