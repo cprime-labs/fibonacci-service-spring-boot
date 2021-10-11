a4787cc506f04b46683b8658538b42791ad9f89c
Kevin Ryan <kevin@pulpdigital.com>
2021-10-11 12:32:07 +0000

    formatting for legibility

--- src/test/java/com/cprime/fibonacciservice/FibonacciCalculatorTests.java
+++ src/test/java/com/cprime/fibonacciservice/FibonacciCalculatorTests.java
@@ -11,44 +11,45 @@ class FibonacciCalculatorTests {
 	public void contextLoads() {
 	}
 
-
 	@Test
-	void zeroIndex() throws FibonacciIdexOutOfBoundsException  {
-        Assertions.assertEquals(0,FibonacciCalculator.calculateFiboacciNumberByIndex(0).getFibonacciValue());
+	void zeroIndex() throws FibonacciIdexOutOfBoundsException {
+		Assertions.assertEquals(0, FibonacciCalculator.calculateFiboacciNumberByIndex(0).getFibonacciValue());
 	}
 
 	@Test
-	void oneIndex() throws FibonacciIdexOutOfBoundsException  {
-        Assertions.assertEquals(1,FibonacciCalculator.calculateFiboacciNumberByIndex(1).getFibonacciValue());
+	void oneIndex() throws FibonacciIdexOutOfBoundsException {
+		Assertions.assertEquals(1, FibonacciCalculator.calculateFiboacciNumberByIndex(1).getFibonacciValue());
 	}
 
-		@Test
+	@Test
 	void twentyFiveIndex() throws FibonacciIdexOutOfBoundsException {
-        Assertions.assertEquals(75025L,FibonacciCalculator.calculateFiboacciNumberByIndex(25).getFibonacciValue());
+		Assertions.assertEquals(75025L, FibonacciCalculator.calculateFiboacciNumberByIndex(25).getFibonacciValue());
 	}
 
 	@Test
 	void fiftyIndex() throws FibonacciIdexOutOfBoundsException {
-        Assertions.assertEquals(12586269025L,FibonacciCalculator.calculateFiboacciNumberByIndex(50).getFibonacciValue());
+		Assertions.assertEquals(12586269025L,
+				FibonacciCalculator.calculateFiboacciNumberByIndex(50).getFibonacciValue());
 	}
 
 	@Test
 	void seventyFiveIndex() throws FibonacciIdexOutOfBoundsException {
-        Assertions.assertEquals(2111485077978050L,FibonacciCalculator.calculateFiboacciNumberByIndex(75).getFibonacciValue());
+		Assertions.assertEquals(2111485077978050L,
+				FibonacciCalculator.calculateFiboacciNumberByIndex(75).getFibonacciValue());
 	}
 
 	@Test
 	void seventySixIndex() {
-        Assertions.assertThrows(FibonacciIdexOutOfBoundsException.class, () -> {
+		Assertions.assertThrows(FibonacciIdexOutOfBoundsException.class, () -> {
 			FibonacciCalculator.calculateFiboacciNumberByIndex(76);
-		  });
+		});
 	}
 
 	@Test
 	void minusOneIndex() {
-        Assertions.assertThrows(FibonacciIdexOutOfBoundsException.class, () -> {
+		Assertions.assertThrows(FibonacciIdexOutOfBoundsException.class, () -> {
 			FibonacciCalculator.calculateFiboacciNumberByIndex(-1);
-		  });
+		});
 	}
 
 }
