d90978537fef3ae265d7133949891671aba49432
Kevin Ryan <kevin@pulpdigital.com>
2021-10-14 06:39:39 +0000

    add api error

---
+++ src/main/java/com/cprime/fibonacciservice/apierror/ApiError.java
@@ -0,0 +1,35 @@
+package com.cprime.fibonacciservice.apierror;
+
+import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
+
+public class ApiError {
+
+    private HttpStatus status;
+    private LocalDateTime timestamp;
+    private String message;
+    private String debugMessage;
+ 
+    private ApiError() {
+        timestamp = LocalDateTime.now();
+    }
+ 
+    ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+ 
+    ApiError(HttpStatus status, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = "Unexpected error";
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+ 
+    ApiError(HttpStatus status, String message, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = message;
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+}
