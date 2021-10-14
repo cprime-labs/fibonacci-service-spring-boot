ae2dd7770be55a175270e5c9bbe9d7e00e139bd7
Kevin Ryan <kevin@pulpdigital.com>
2021-10-14 11:08:24 +0000

    add custom RestExceptionHandler

--- src/main/java/com/cprime/fibonacciservice/apierror/ApiError.java
+++ src/main/java/com/cprime/fibonacciservice/apierror/ApiError.java
@@ -9,27 +9,32 @@ public class ApiError {
     private LocalDateTime timestamp;
     private String message;
     private String debugMessage;
+
+    public HttpStatus getStatus() {
+        return status;
+    }
  
     private ApiError() {
         timestamp = LocalDateTime.now();
     }
  
-    ApiError(HttpStatus status) {
+    public ApiError(HttpStatus status) {
         this();
         this.status = status;
     }
  
-    ApiError(HttpStatus status, Throwable ex) {
+    public ApiError(HttpStatus status, Throwable ex) {
         this();
         this.status = status;
         this.message = "Unexpected error";
         this.debugMessage = ex.getLocalizedMessage();
     }
  
-    ApiError(HttpStatus status, String message, Throwable ex) {
+    public ApiError(HttpStatus status, String message, Throwable ex) {
         this();
         this.status = status;
         this.message = message;
         this.debugMessage = ex.getLocalizedMessage();
     }
+
 }
