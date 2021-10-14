e4d4f86e868422dbdcabacdce155f42e07456e94
Kevin Ryan <kevin@pulpdigital.com>
2021-10-14 11:51:39 +0000

    add custom api error handlers

--- src/main/java/com/cprime/fibonacciservice/RestExceptionHandler.java
+++ src/main/java/com/cprime/fibonacciservice/RestExceptionHandler.java
@@ -10,19 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.core.Ordered;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.context.request.WebRequest;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String error = "Malformed JSON request";
-        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
-    }
  
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
